61f22ed74d9e94811aa6f70b1699ca9c72ea6c2f
ArjayGwapo <150806881+Arjay4545@users.noreply.github.com>
2025-10-21 00:02:53 +0800

    Add broadcast events, echo client and docs

---
+++ app/Events/RequestCreated.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class RequestCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $payload;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        // Broadcast to a private channel for the user (employee)
+        $employeeId = data_get($this->payload, 'employee_id');
+        return new PrivateChannel("user.history.{$employeeId}");
+    }
+
+    public function broadcastWith()
+    {
+        return $this->payload;
+    }
+}
